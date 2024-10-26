e3d5acc88185d7f3408b3f9ab28b75bc10a842bd
NEERJA BINU VIMALAN <85398511+Ninjha001@users.noreply.github.com>
2024-10-26 19:07:12 +0530

    Delete SampleProject directory

--- SampleProject/src/com/sampleproject/www/SampleDemo.java
+++
@@ -1,9 +0,0 @@
-package com.sampleproject.www;
-
-public class SampleDemo {
-
-	public static void main(String[] args) {
-		System.out.println("Sample Demo Project");
-	}
-
-}
--- SampleProject/src/com/sampleproject/www/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.sampleproject.www;
